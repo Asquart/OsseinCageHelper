6e5963813346367298a71cd6f22eab5923daf528
Asquart <ycyack@gmail.com>
2025-06-16 17:02:29 +0100

    Update 1.8.2

--- AsquartOsseinCageHelper/AsquartOsseinCageHelper.lua
+++ AsquartOsseinCageHelper/AsquartOsseinCageHelper.lua
@@ -2,7 +2,7 @@ AOCH = AOCH or {}
 local AOCH = AOCH
 
 AOCH.name     = "AsquartOsseinCageHelper"
-AOCH.version  = "1.8.1"
+AOCH.version  = "1.8.2"
 AOCH.author   = "|c24abfe@Asquart|r & |cbb00ff@Margorius|r"
 AOCH.active   = false
 
